76a50ccebe0d784897e915947824c08d2352ebc9
Daiyrbek Artelov <4unkur@mail.ru>
2018-11-28 19:58:13 +0600

    Code style improvements

--- resources/js/components/VideoChat.vue
+++ resources/js/components/VideoChat.vue
@@ -44,67 +44,58 @@
             this.channel = Echo.join(`interview.${this.chat.hash}`)
 
             this.channel
-                .here((users) => {
+                .here(users => {
                     this.id = this.channel.pusher.channels.channels[this.channel.name].members.me.id
-                    users.forEach((user) => {
+                    users.forEach(user => {
                         if (user.id != this.id) {
                             this.users.push({id: user.id, name: user.name})
                         }
                     });
                 })
-                .joining((user) => {
-                    this.users.push({id: user.id, name: user.name})
-                })
-                .leaving((user) => {
+                .joining(user => this.users.push({id: user.id, name: user.name}))
+                .leaving(user => {
                     let index = this.users.indexOf(user.id);
                     this.users.splice(index, 1);
                     if (user.id == this.room) {
                         this.endCall()
                     }
                 })
-
                 .listenForWhisper("client-candidate", message => {
                     //Listening for the candidate message from a peer sent from onicecandidate handler
                     if (message.room == this.room) {
-                        // I have no idea what this shit does, so I commented it
                         this.caller.addIceCandidate(new RTCIceCandidate(message.candidate))
                     }
                 })
-                .listenForWhisper("client-sdp", message => {
+                .listenForWhisper("client-sdp", async message => {
                     //Listening for Session Description Protocol message with session details from remote peer
-
                     if (message.room != this.id) {
                         return;
                     }
 
                     console.log("sdp received");
-                    let answer = confirm(`You have a call from: ${message.from}. Would you like to answer?`);
-                    if (!answer) {
+                    let confirmed = confirm(`You have a call from: ${message.from}. Would you like to answer?`);
+                    if (!confirmed) {
                         return this.channel.whisper("client-reject", {"room": message.room, "rejected": this.id});
                     }
 
                     this.room = message.room;
-                    this.caller.setRemoteDescription(message.sdp).then(() => {
-                        this.getCamera().then(stream => {
-                            this.localUserMedia = stream;
-                            this.toggleEndCallButton();
-                            this.$refs.selfview.srcObject = stream
-                            stream.getTracks().forEach((track) => {
-                                this.caller.addTrack(track, stream);
-                            });
-                            if (this.caller.remoteDescription.type === 'offer') {
-                                this.caller.createAnswer().then(answer => {
-                                    this.caller.setLocalDescription(answer);
-                                    this.channel.whisper("client-answer", {
-                                        "sdp": answer,
-                                        "room": this.room
-                                    });
-                                })
-                            }
-                        }).catch(error => {
-                            console.log('an error occured', error);
-                        })
-                    })
+                    await this.caller.setRemoteDescription(message.sdp)
+                    let stream = await this.getCamera()
+                    this.localUserMedia = stream;
+                    this.toggleEndCallButton();
+                    this.$refs.selfview.srcObject = stream
+                    stream.getTracks().forEach(track => {
+                        this.caller.addTrack(track, stream);
+                    });
+                    if (this.caller.remoteDescription.type !== 'offer') {
+                        return;
+                    }
+                    let answer = await this.caller.createAnswer()
+                    this.caller.setLocalDescription(answer);
+                    this.channel.whisper("client-answer", {
+                        "sdp": answer,
+                        "room": this.room
+                    });
                 })
                 .listenForWhisper("client-answer", answer => {
                     if (answer.room == this.room) {
@@ -154,30 +145,23 @@
                 });
             },
             //Create and send offer to remote peer on button click
-            callUser(user) {
-                this.getCamera()
-                    .then(stream => {
-                        this.$refs.selfview.srcObject = stream
-                        this.toggleEndCallButton();
-                        // this.caller.addStream(stream);
-                        stream.getTracks().forEach((track) => {
-                            this.caller.addTrack(track, stream);
-                        });
-                        this.localUserMedia = stream;
-                        this.caller.createOffer().then(offer => {
-                            this.caller.setLocalDescription(offer).then(() => {
-                                this.channel.whisper("client-sdp", {
-                                    "sdp": offer,
-                                    "room": user.id,
-                                    "from": this.id
-                                });
-                                this.room = user.id;
-                            })
-                        });
-                    })
-                    .catch(error => {
-                        console.log('an error occurred', error);
-                    })
+            async callUser(user) {
+                let stream = await this.getCamera()
+                this.$refs.selfview.srcObject = stream
+                this.toggleEndCallButton();
+                // this.caller.addStream(stream);
+                stream.getTracks().forEach((track) => {
+                    this.caller.addTrack(track, stream);
+                });
+                this.localUserMedia = stream;
+                let offer = await this.caller.createOffer()
+                await this.caller.setLocalDescription(offer)
+                this.channel.whisper("client-sdp", {
+                    "sdp": offer,
+                    "room": user.id,
+                    "from": this.id
+                });
+                this.room = user.id;
             },
             endCall() {
                 this.room = null;
